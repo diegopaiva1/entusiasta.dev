fe991da7cd4507967bfd817a0bf892214b30373f
Diego Paiva <diego.paiva13@gmail.com>
2021-10-02 02:14:22 -0300

    Versão melhorada

--- config/database.php
+++ config/database.php
@@ -59,7 +59,7 @@ return [
             'port'       => 3306,
             'database'   => 'blog',
             'username'   => 'diego',
-            'password'   => '20642064',
+            'password'   => 'd20646497O',
             'charset'    => 'utf8mb4',
             'collation'  => 'utf8mb4_unicode_ci',
             'prefix'     => '',
